985237d1f10891c79193dd030d71c151ea9ff809
Sam Thomson <samuelthomson62@gmail.com>
2019-09-10 19:45:25 -0400

    added Player.cs to modify the player a bit

---
+++ ChooseYourOwnAdventure/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChooseYourOwnAdventure
+{
+    class Player
+    {
+        public double Health { get; set; }
+        public string Name { get; set; }
+        public string Weapon { get; set; }
+    }
+}
